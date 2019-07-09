9d687d6f0c3bdd2d8ac970862d1e393b65ed8586
Michael Hermon <michaelhermon@Michael-Her-MBP.local>
2019-07-10 00:42:38 +0300

    added support to check multiple search queries

--- scrape.py
+++ scrape.py
@@ -8,8 +8,8 @@ def telegram_bot_sendtext(bot_message):
     bot_chatID = os.environ.get('bot_chatID')
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
-
     return response.json()
+
 
 try:
     DATABASE_URL = os.environ['DATABASE_URL']
@@ -17,6 +17,9 @@ try:
     cursor = conn.cursor()
     cursor.execute("SELECT id from apts;")
     viewedapts = cursor.fetchall()
+
+    cursor.execute("SELECT query from queries;")
+    queries = cursor.fetchall()
 
 except (Exception, psycopg2.Error) as error:
     print ("Error while fetching data from PostgreSQL", error)
@@ -29,34 +32,43 @@ apts = []
 for row in viewedapts:
     apts.append(row[0])
 
+querylist = []
+for row in queries:
+    querylist.append(row[0])
+
+print("queries retrieved: ")
+print(querylist)
+
 print("list retrieved: ")
 print(apts)
 
-URL = os.environ.get('search_url')
-try:
-    r = requests.get(url = URL, headers={"USER-AGENT":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"})
-except:
-    sys.exit("Error getting data from yad2")
-
-data = r.json()
 aptcount = 0
 
-for apt in data["feed"]["feed_items"]:
-    if 'row_1' in apt and apt["id"] not in apts:
-        aptcount+=1
-        print(apt["row_1"]+": "+apt["id"])
-        try:
-            print("Sending msg via bot")
-            print(telegram_bot_sendtext(apt["row_1"]+"\n"+apt["row_2"]+"\n"+apt["price"]+"\n"+"https://www.yad2.co.il/item/"+apt["id"]))
-        except:
-            print("Failed to send message with description, sending link only")
-            print(telegram_bot_sendtext("https://www.yad2.co.il/item/" + apt["id"]))
+for URL in querylist:
+    try:
+        r = requests.get(url = URL, headers={"USER-AGENT":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"})
+    except:
+        sys.exit("Error getting data from yad2")
 
-        try:
-            cursor.execute("INSERT INTO apts (id) VALUES ('"+apt["id"]+"');")
-            conn.commit()
-        except:
-            print(telegram_bot_sendtext("Error inserting apt into DB"))
-            sys.exit("Error inserting apt into DB")
+    data = r.json()
+
+    for apt in data["feed"]["feed_items"]:
+        if 'row_1' in apt and apt["id"] not in apts:
+            aptcount+=1
+            print(apt["row_1"]+": "+apt["id"])
+            try:
+                print("Sending msg via bot")
+                print(telegram_bot_sendtext(apt["row_1"]+"\n"+apt["row_2"]+"\n"+apt["price"]+"\n"+"https://www.yad2.co.il/item/"+apt["id"]))
+            except:
+                print("Failed to send message with description, sending link only")
+                print(telegram_bot_sendtext("https://www.yad2.co.il/item/" + apt["id"]))
+
+            try:
+                cursor.execute("INSERT INTO apts (id) VALUES ('"+apt["id"]+"');")
+                conn.commit()
+                apts.append(apt["id"])
+            except:
+                print(telegram_bot_sendtext("Error inserting apt into DB"))
+                sys.exit("Error inserting apt into DB")
 
 print("Done processing. Found ",aptcount," new relevant listings")
