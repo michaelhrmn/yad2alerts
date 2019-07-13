011df7d072e5f72aebe163a3733fdcc8a2f9d240
Michael Hermon <michaelhermon@Michael-Her-MBP.local>
2019-07-13 18:45:25 +0300

    bug fix

--- server.py
+++ server.py
@@ -49,6 +49,6 @@ class SimpleHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     try:
-        BaseHTTPServer.HTTPServer(("0.0.0.0", 80), SimpleHandler).serve_forever()
+        BaseHTTPServer.HTTPServer(("0.0.0.0", 5000), SimpleHandler).serve_forever()
     except KeyboardInterrupt:
         print('shutting down server')
