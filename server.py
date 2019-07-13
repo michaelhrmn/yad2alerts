2a6200e751231b5acaf79dbe6b7d6434bc6d2d67
Michael Hermon <michaelhermon@Michael-Her-MBP.local>
2019-07-13 18:41:55 +0300

    bug fix

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ class SimpleHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             queryString = "https://www.yad2.co.il/api/pre-load/getFeedIndex/realestate/rent?";
 
             for key,value in dict(urlparse.parse_qsl(self.path.split("?")[1], True)).items():
-                print key + " = " + value
+                print(key + " = " + value)
                 if value:
                     queryString+= "&" + key + "=" + value
 
@@ -42,7 +42,7 @@ class SimpleHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         if self.rfile:
              # print urlparse.parse_qs(self.rfile.read(int(self.headers['Content-Length'])))
              for key,value in dict(urlparse.parse_qs(self.rfile.read(int(self.headers['Content-Length'])))).items():
-                 print key + " = " + value[0]
+                 print(key + " = " + value[0])
 
     def log_request(self, code=None, size=None):
         return
